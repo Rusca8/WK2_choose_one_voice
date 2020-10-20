80daac71f83fc7a0709a0000fac72d8773d45b72
David Ruscalleda <rusca.dev@gmail.com>
2020-10-20 11:03:58 +0200

    Corrections for cards with multiple audios
    A small sample of the cards have more than 2 audios. This new version takes that into account.

--- choose_one_voice/__init__.py
+++ choose_one_voice/__init__.py
@@ -5,6 +5,8 @@ from aqt.utils import showInfo, showWarning
 # import all of the Qt GUI library
 from aqt.qt import *
 
+import random
+
 
 def ask_which():
     msg = QMessageBox()
@@ -26,28 +28,39 @@ def which_btn(i):
 
     ids = mw.col.findNotes("deck:current")  # get id for all notes on the current deck
     # counting audios
-    count = 0
+    counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     cdouble = 0
+    cdnew = 0
     for note_id in ids:
         note = mw.col.getNote(note_id)  # get note from the given id
         if "Audio" in note:
             if "[" in note["Audio"]:
-                count += 1
-                if "][" in note["Audio"]:
+                audios = note["Audio"].count("]")  # how many audios are there?
+                counts[min(audios-1, 8)] += 1
+                if not audios % 2:  # allegedly pairs of Female-Male
                     cdouble += 1
+                    if "·" not in note["Audio"]:  # not-processed-before pairs
+                        cdnew += 1
 
     # message box choose male vs female
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Warning)
-    text = ("<b>Deck: '%s'</b><br>"
-            "· %s notes with audio<br>"
-            "· %s notes with both audios<br><br>" % (deckname, count, cdouble))
+    text = "<b>Deck: '%s'</b><br><br>Content:<br>" % deckname
+    for n, x in enumerate(counts):
+        if n == 8:
+            text += "· %s notes with %s+ audios<br>" % (x, (n + 1))
+        elif n == 0:
+            text += "· %s notes with %s audio<br>" % (x, (n + 1))
+        else:
+            text += "· %s notes with %s audios<br>" % (x, (n+1))
+    text += ("<br>Analysis:<br>· %s notes with audio pairs<br>· %s of those %s were never processed<br><br>"
+             % (cdouble, cdnew, cdouble))
     if i.text() == "Female":  # keep female
         text += ("We will delete the <b>male</b> sounds from %s notes.<br><br>"
-                 "<i>This assumes the Female sound is the first one you hear.</i>" % cdouble)
+                 "<i>This assumes the Female sound is the first one you hear.</i>" % cdnew)
     elif i.text() == "Male":  # keep male
         text += ("We will delete the <b>Female</b> sounds from %s notes.<br><br>"
-                 "<i>This assumes the Male sound is the second one you hear.</i>" % cdouble)
+                 "<i>This assumes the Male sound is the second one you hear.</i>" % cdnew)
     msg.setText("The deletion will be permanent.")
     msg.setInformativeText(text)
     msg.setWindowTitle("CONFIRM PERMANENT ERASING")
@@ -65,7 +78,7 @@ def which_btn(i):
     else:
         showInfo("Something weird happened (Err: which_btn)")
 
-    if count:
+    if any(x for x in counts):
         msg.exec_()  # show message box
     else:
         showWarning("I don't think you can do this here,<br>my friend...<br><br>Expected audios in 'Audio' field.<br>"
@@ -99,18 +112,26 @@ def erase(delete="Male"):
     for note_id in ids:
         note = mw.col.getNote(note_id)  # get note from the given id
         if "Audio" in note:
-            if "][" in note["Audio"]:
-                a_female, a_male = note["Audio"].split("]")[:2]
-                a_female += "]"
-                a_male += "]"
-                # keep the chosen one
-                if delete == "Male":
-                    note["Audio"] = a_female
-                elif delete == "Female":
-                    note["Audio"] = a_male
-                note.flush()
+            if "[" in note["Audio"] and "·" not in note["Audio"]:  # already processed pairs will have a "·"
+                count = note["Audio"].count("]")  # how many audios are there?
+                if not count % 2:  # even number of audios
+                    audios = note["Audio"].split("]")
+                    for x in range(len(audios)):  # get the "]" back
+                        if audios[x]:
+                            audios[x] += "]"
+                    a_female = "·".join(audios[x] for x in range(len(audios)) if not x % 2)  # even audios
+                    a_male = "·".join(audios[x] for x in range(len(audios)) if x % 2)  # odd audios
+                    if a_female.endswith("·"):  # delete the last "·", which I don't need
+                        a_female = a_female[:-1]
+                    if a_male.endswith("·"):
+                        a_male = a_female[:-1]
+                    # keep the chosen one
+                    if delete == "Male":
+                        note["Audio"] = a_female
+                    elif delete == "Female":
+                        note["Audio"] = a_male
+                    note.flush()
     showInfo("Deletion completed.")
-    # showInfo("Last note<br>%s<br>%s" % (a_female, a_male))
 
 # create a new menu item
 action = QAction('Choose one voice (WK2)', mw)
